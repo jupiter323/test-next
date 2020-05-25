1ab57be49a64120b97c219626a8a7692ac298960
jupiter <jupiter.fierce.31@gmail.com>
2020-05-25 18:56:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Example of the simple paggination in Next.js using `react-paginate`
+Full tutorial: https://vpilip.com/how-build-simple-pagination-in-nextjs/
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
---
+++ src/pages/index.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import Router, { withRouter } from 'next/router'
+
+import '../assets/styles.scss';
+
+
+const Home = (props) => {
+    const [isLoading, setLoading] = useState(false);
+    const startLoading = () => setLoading(true);
+    const stopLoading = () => setLoading(false);
+
+    useEffect(() => {
+        Router.events.on('routeChangeStart', startLoading);
+        Router.events.on('routeChangeComplete', stopLoading);
+
+        return () => {
+            Router.events.off('routeChangeStart', startLoading);
+            Router.events.off('routeChangeComplete', stopLoading);
+        }
+    }, [])
+
+    const pagginationHandler = (page) => {
+        const currentPath = props.router.pathname;
+        const currentQuery = { ...props.router.query };
+        currentQuery.page = page.selected + 1;
+
+        props.router.push({
+            pathname: currentPath,
+            query: currentQuery,
+        });
+
+    };
+
+    let content = null;
+    if (isLoading)
+        content = <div>Loading...</div>;
+    else {
+        content = (
+            <ul>
+                {props.posts.map(post => {
+                    return <li key={post.id}>{post.title}</li>;
+                })}
+            </ul>
+        );
+    }
+
+    return (
+        <div className="container">
+            <h1>Lessons with Pagination in Next.js</h1>
+            <div className="posts">
+                {content}
+            </div>
+
+            <ReactPaginate
+                previousLabel={'previous'}
+                nextLabel={'next'}
+                breakLabel={'...'}
+                breakClassName={'break-me'}
+                activeClassName={'active'}
+                containerClassName={'pagination'}
+                subContainerClassName={'pages pagination'}
+
+                initialPage={props.currentPage - 1}
+                pageCount={props.pageCount} //page count
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={pagginationHandler}
+            />
+        </div>
+    );
+};
+
+Home.getInitialProps = async ({ query }) => {
+    const page = query.page || 1;
+    const url = `http://dev.api.clearchoiceprep.info/api/lessons?page=${page}`
+    const config = {
+        headers: {
+          'X-Dev-User-Id': '53f24b85-94da-4bf2-882b-3ff5ca16d86f'
+        }
+      }
+      
+    const posts = await axios.get(url, null, config);
+    return {
+        pageCount: posts.data.pages,
+        currentPage: posts.data.current_page,
+        posts: posts.data.lessons,
+        isLoading: false,
+    };
+}
+
+
+export default withRouter(Home);
